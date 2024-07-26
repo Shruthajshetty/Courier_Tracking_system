3a623512317eba371f81205a3914e1fd7028283d
Shruthajshetty <109536307+Shruthajshetty@users.noreply.github.com>
2024-07-26 23:45:56 +0530

    Update Conn.java

--- src/courier/tracking/system/Conn.java
+++ src/courier/tracking/system/Conn.java
@@ -13,28 +13,28 @@ import java.sql.DriverManager;
 import java.sql.Statement;
 
 public class Conn {
-    // Fields to store the connection and statement objects
+   
     private Connection connection;
     private Statement statement;
 
-    // Constructor to establish the database connection
+    
     public Conn() {
         try {
-            // Load the MySQL JDBC driver
+          
             Class.forName("com.mysql.cj.jdbc.Driver");
             
-            // Establish a connection to the database
+            
             connection = DriverManager.getConnection("jdbc:mysql://localhost:3306/courier_db", "root", "SHRU@04");
             
-            // Create a statement object for executing SQL queries
+           
             statement = connection.createStatement();
         } catch (Exception e) {
-            // Print any exceptions that occur during connection setup
+          
             e.printStackTrace();
         }
     }
     
-    // Getter methods to retrieve the connection and statement objects
+ 
     public Connection getConnection() {
         return connection;
     }
